785ce3db2b36ce5d909a3cf1b4190eb2d765dcc6
Omer Hasanov <ka9an1sn00b@gmail.com>
2026-05-01 14:35:51 +0200

    try new hat xy values

--- controller.cpp
+++ controller.cpp
@@ -191,19 +191,19 @@ void ControllerManager::emulateJoystick(byte controller_index, byte button_count
 
 
     if(controllers[controller_index - 1].button_states[DPAD_UP]){
-        sendEvent(fd, EV_ABS, ABS_HAT0Y, -32768);
+        sendEvent(fd, EV_ABS, ABS_HAT0Y, 1);
     }
     else if(controllers[controller_index - 1].button_states[DPAD_DOWN]){
-        sendEvent(fd, EV_ABS, ABS_HAT0Y, 32767);
+        sendEvent(fd, EV_ABS, ABS_HAT0Y, -1);
     }
     else {
         sendEvent(fd, EV_ABS, ABS_HAT0Y, 0);
     }
     if(controllers[controller_index - 1].button_states[DPAD_LEFT]){
-        sendEvent(fd, EV_ABS, ABS_HAT0X, -32768);
+        sendEvent(fd, EV_ABS, ABS_HAT0X, 1);
     }
     else if(controllers[controller_index - 1].button_states[DPAD_RIGHT]){
-        sendEvent(fd, EV_ABS, ABS_HAT0X, 32767);
+        sendEvent(fd, EV_ABS, ABS_HAT0X, -1);
     }
     else{
         sendEvent(fd, EV_ABS, ABS_HAT0X, 0);
